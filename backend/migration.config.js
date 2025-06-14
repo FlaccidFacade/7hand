a671a749e4cb3594f839e32d618df52093fda9b2
FlaccidFacade <zachary.glenn.turner@gmail.com>
2025-06-14 15:30:04 -0500

    fix migration logic

--- backend/migration.config.js
+++ backend/migration.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   migrationFolder: './migrations',
   direction: 'up',
-  databaseUrl: process.env.DATABASE_URL || 'postgres://cardgame:cardgamepass@db:5432/cardgamedb',
+  databaseUrl:
+    process.env.DATABASE_URL ||
+    `postgres://${process.env.DB_USER || 'cardgame'}:${process.env.DB_PASSWORD || 'cardgamepass'}@${process.env.DB_HOST || 'db'}:${process.env.DB_PORT || 5432}/${process.env.DB_NAME || 'cardgamedb'}`,
 };
